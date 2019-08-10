a20252e75470dfa72dbec4a73d8c3ef8c991ab1d
amasshar <amasshar@AMASSHAR-IN.oradev.oraclecorp.com>
2019-08-10 12:00:03 +0530

    endpoints

--- src/main/java/com/dbshacktron/backend/controllers/SimpleController.java
+++ src/main/java/com/dbshacktron/backend/controllers/SimpleController.java
@@ -1,7 +1,14 @@
 package com.dbshacktron.backend.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dbshacktron.backend.models.Queue;
 
 @RestController
 public class SimpleController {
@@ -10,4 +17,28 @@ public class SimpleController {
     public String helloWorld() {
         return "Welcome to Hacktron";
     }
+    
+    @PostMapping("/createQueue")
+    public ResponseEntity<String> createQueue(@RequestBody Queue queue ) {
+    	
+    	return null;
+    }
+    
+    @DeleteMapping("/consumeQueue/{messageId}")
+    public void removeFromQueue(String messageId) {
+    	
+    }
+    
+    @GetMapping("/getMessage")
+    public String browseMessage() {
+    	
+    	return "";
+    } 
+   
+    @DeleteMapping("/clearQueue")
+    public ResponseEntity<String> clearQueue(){
+    	
+    	return null;
+    }
+    
 }
