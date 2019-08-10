4ed5629ac84a3f7a4766c271a537e4013d8fb095
SONY <SONY@SONY-PC>
2019-08-10 11:27:36 +0530

    added the model classes

---
+++ src/main/java/com/dbshacktron/model/Queue.java
@@ -0,0 +1,25 @@
+package com.dbshacktron.model;
+
+import java.util.List;
+
+public class Queue {
+	private String queueName;
+	private List<Message> messages;
+
+	public String getQueueName() {
+		return queueName;
+	}
+
+	public void setQueueName(String queueName) {
+		this.queueName = queueName;
+	}
+
+	public List<Message> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(List<Message> messages) {
+		this.messages = messages;
+	}
+
+}
