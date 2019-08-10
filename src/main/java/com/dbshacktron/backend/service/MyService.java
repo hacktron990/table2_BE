36835748a02b735b0e7e9cc3f1fa888150a9aafc
SONY <SONY@SONY-PC>
2019-08-10 12:12:55 +0530

    Resolved conflicts

---
+++ src/main/java/com/dbshacktron/backend/service/MyService.java
@@ -0,0 +1,26 @@
+package com.dbshacktron.backend.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.dbshacktron.model.Message;
+import com.dbshacktron.model.Queue;
+
+public class MyService {
+	private Map<String, Queue> map = new HashMap<>();
+
+	public void storeMessages(String queueName,List<Message> messages) {
+		if (map.containsKey(queueName)) {
+			Queue queue = map.get(queueName);
+			queue.setMessages(messages);
+		} else {
+			Queue queue =new Queue();
+			queue.setMessages(messages);
+			map.put(queueName, queue);
+		}
+	}
+	public List<Message> getMessages(String messages){
+		
+	}
+}
