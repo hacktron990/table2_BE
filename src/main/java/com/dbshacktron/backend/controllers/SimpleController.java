1070fce3abad7aa57dd63bbb8b35df52e7044e5a
amasshar <amasshar@AMASSHAR-IN.oradev.oraclecorp.com>
2019-08-10 12:10:15 +0530

    End Points

--- src/main/java/com/dbshacktron/backend/controllers/SimpleController.java
+++ src/main/java/com/dbshacktron/backend/controllers/SimpleController.java
@@ -1,5 +1,7 @@
 package com.dbshacktron.backend.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dbshacktron.backend.models.Message;
 import com.dbshacktron.backend.models.Queue;
 
 @RestController
@@ -18,27 +21,39 @@ public class SimpleController {
         return "Welcome to Hacktron";
     }
     
-    @PostMapping("/createQueue")
+    @PostMapping("/queue")
     public ResponseEntity<String> createQueue(@RequestBody Queue queue ) {
-    	
+
     	return null;
     }
     
-    @DeleteMapping("/consumeQueue/{messageId}")
-    public void removeFromQueue(String messageId) {
+    @PostMapping("/message")
+    public ResponseEntity<String> createMeassge(@RequestBody Message message ) {
+
+    	return null;
+    }
+    
+    @DeleteMapping("/queue/{queueId}/message/{messageId}")
+    public void removeFromQueue(String queueId, String messageId) {
     	
     }
     
-    @GetMapping("/getMessage")
-    public String browseMessage() {
+    @GetMapping("/queue/{queueId}/message/{messageId}")
+    public Message browseMessage() {
     	
-    	return "";
+    	return new Message();
     } 
    
-    @DeleteMapping("/clearQueue")
+    @DeleteMapping("/queue/{queueId}")
     public ResponseEntity<String> clearQueue(){
     	
     	return null;
     }
+    
+    @GetMapping("/queue/{queueId}/message")
+    public List<Message> ListOfMessage() {
+    	
+    	return null;
+    } 
     
 }
